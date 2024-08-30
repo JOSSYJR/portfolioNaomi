b3507aa3b2eb90199a9427c14b4374c3c5acea0c
Yosef Tesfaye <yoseftesfaye364@gmail.com>
2024-08-30 21:42:26 +0300

    to be removed if production doesnt work 2

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: './icon.svg',
-    brandTitle: 'Hamish Williams Components',
+    brandTitle: 'Robel Ephrem Components',
     brandUrl: 'https://hamishw.com',
   },
 });
