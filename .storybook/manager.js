af2564b5478b6dd5490eeb3bd687306c0fdd8e28
Yosef Tesfaye <yoseftesfaye364@gmail.com>
2024-08-30 21:53:45 +0300

    Revert "to be removed if production doesnt work 2"
    This reverts commit b3507aa3b2eb90199a9427c14b4374c3c5acea0c.

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: './icon.svg',
-    brandTitle: 'Robel Ephrem Components',
+    brandTitle: 'Hamish Williams Components',
     brandUrl: 'https://hamishw.com',
   },
 });
